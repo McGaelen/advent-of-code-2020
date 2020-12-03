374178749d1e778891ca0c10a1a2407b7d0feee8
Gaelen McIntee <drumbum1098@gmail.com>
2020-12-02 23:23:15 -0500

    init commit

---
+++ 1/main.rb
@@ -0,0 +1,35 @@
+$numbers = []
+file = open('input.txt')
+
+file.each do |thing|
+  $numbers.push thing.to_i
+end
+
+# pt 1
+def part1
+  $numbers.each do |num1|
+    $numbers.each do |num2|
+      if num1 + num2 == 2020
+        puts "#{num1 * num2}"
+        return
+      end
+    end
+  end
+end
+
+# pt 2
+def part2
+  $numbers.each do |num1|
+    $numbers.each do |num2|
+      $numbers.each do |num3|
+        if num1 + num2 + num3 == 2020
+          puts "#{num1 * num2 * num3}"
+          return
+        end
+      end
+    end
+  end
+end
+
+# part1
+part2
