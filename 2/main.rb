4789a275ce264fca55e73b34d1b642e370f8cd0d
Gaelen McIntee <drumbum1098@gmail.com>
2020-12-09 15:20:07 -0500

    finish day 2 part 2

--- 2/main.rb
+++ 2/main.rb
@@ -33,4 +33,23 @@ def part1
   puts numCorrect
 end
 
-part1
+def part2
+  numCorrect = 0
+
+  $passwords.each do |pass|
+    password = pass['password']
+    pos1 = pass['atLeast'] - 1
+    pos2 = pass['atMost'] - 1
+
+    positions = password.enum_for(:scan, pass['regex']).map do
+      Regexp.last_match.begin(0)
+    end
+
+    # XOR the positions
+    numCorrect += 1 if positions.include?(pos1) ^ positions.include?(pos2)
+  end
+
+  puts numCorrect
+end
+
+part2
