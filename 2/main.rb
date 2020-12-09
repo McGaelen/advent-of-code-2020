098cbce4f5b026b2e278a9c836a81df1997b0371
Gaelen McIntee <drumbum1098@gmail.com>
2020-12-09 09:53:57 -0500

    fix day 2 pt 1

--- 2/main.rb
+++ 2/main.rb
@@ -7,23 +7,31 @@ file.each do |line|
   passDetails = line.split ' '
 
   uses = passDetails[0].split '-'
-  atLeast = uses[0]
-  atMost = uses[1]
+  atLeast = uses[0].to_i
+  atMost = uses[1].to_i
   letter = passDetails[1].chomp ':'
-  # [letter]{atLeast,atMost}
-  $regexes.push Regexp.new("[#{letter}]{#{atLeast},#{atMost}}")
+  $regexes.push Regexp.new("[#{letter}]")
 
   # password
   password = passDetails[2]
-  $passwords.push password
+  $passwords.push({
+    'password' => password,
+    'atLeast' => atLeast,
+    'atMost' => atMost
+  })
 end
 
+puts $regexes.length
+puts $passwords.length
+
 def part1
-  puts $regexes
   numCorrect = 0
+
   $passwords.each_with_index do |pass, index|
-    numCorrect += 1 if (pass =~ $regexes[index]) != nil
+    matches = pass['password'].scan($regexes[index])
+    numCorrect += 1 if matches.length >= pass['atLeast'] and matches.length <= pass['atMost']
   end
+
   puts numCorrect
 end
 
